ae82be0e7aac1b85a1a7aa0fe66aed8252cd24f2
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2021-03-04 02:53:23 -0300

    validation-functions

--- public/index.js
+++ public/index.js
@@ -1,29 +1,88 @@
-const dateButton = document.querySelector('.dateButton')
 const dateInput = document.querySelector('.userDate')
+const dateButton = document.querySelector('.dateButton')
+const actualDateButton = document.querySelector('.actualDateButton')
+
 let today = ''
 
-getTodaysTime()
-dateButton.addEventListener('click', startCountDown)
+dateButton.addEventListener('click', () => {
+  if ( !dateInput.value ) return console.error('without value')
+  settingCountDown()
 
-function getTodaysTime() {
-  const date = new Date()
+})
 
-  const actualFullYear = date.getFullYear()
-  const actualMonth = ((date.getMonth() + 1) < 10 ) ? `0${(date.getMonth() + 1)}` : date.getMonth() + 1
-  const actualDay = ( date.getDate() < 10 ) ? `0${date.getDate()}` : date.getDate()
+actualDateButton.addEventListener('click', getDateNow)
 
-  dateInput.value = `${actualFullYear}-${actualMonth}-${actualDay}`
-  today = `${actualFullYear}-${actualMonth}-${actualDay}`
+function getDateNow() {
+  // * Updating the date of the input
 
+  const date = dateNow()
+  const dateFormat = formatTransformation(date.year, date.month, date.day)
+
+  return dateInput.value = `${dateFormat.validYear}-${dateFormat.validMonth}-${dateFormat.validDay}`
 }
 
-function startCountDown() {
-  const splitDateInput = dateInput.value.split('-')
-  const splitToday = today.split('-')
+function dateNow() {
+  // * Getting the user PC date
+  const date = new Date()
 
-  if ( splitDateInput[0] >= splitToday[0] ) {
-    return console.log('valid')
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
 
-  } else return console.error('invalid')
+  return { year, month, day }
+}
+
+function formatTransformation(year, month, day) {
+  // * Transform the numbers add '0' in front of the numbers below 10
+
+  const validYear = year
+  const validMonth = ( month < 10 ) ? `0${month}` : month
+  const validDay = ( day < 10 ) ? `0${day}` : day
+
+  return { validYear, validMonth, validDay }
+}
+
+function getInputDate(rawDate) {
+  // * Getting the user value in the input
+
+  const dateArray = rawDate.split('-')
+  
+  const year = parseInt(dateArray[0])
+  const month = parseInt(dateArray[1]) 
+  const day = parseInt(dateArray[2])
+
+  return { year, month, day }
+}
+
+function settingCountDown() {
+  const inputValue = getInputDate(dateInput.value)
+  const userDate = dateNow()
+  
+  if((!validateCountDown(inputValue, userDate))) return
+  console.log(inputValue)
+
+
+  function validateCountDown(inputDate, dateNow) {
+    // ? Validate if the user date is after the PC date
+
+    if ( dateNow.year > inputDate.year ) {
+      console.error('invalid year')
+      return false
+    }
+
+    if ( dateNow.month > inputDate.month && dateNow.year >= inputDate.year ) {
+      console.error('invalid month')
+      return false
+
+    }
+
+    if ( dateNow.day > inputDate.day && dateNow.month >= inputDate.month && dateNow.year >= inputDate.year ) {
+      console.error('invalid day')
+      return false
+
+    }
+
+    return true
+  }
 
 }
